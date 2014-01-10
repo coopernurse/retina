ff35ec34280e1fd26c6d0be28cc4e279835fa1c9
James Cooper <jamespcooper@gmail.com>
2014-01-10 12:40:28 -0800

    remove integ/bin/client.go

--- test.sh
+++ test.sh
@@ -4,6 +4,5 @@ set -e
 
 mkdir -p bin
 go build -o bin/retina  retina.go
-go build -o bin/client  ./integ/bin/client.go
 go build -o bin/backend ./integ/bin/backend.go
 go test -v ./integ -gocheck.v
